6895d5aa97d4bc98e30b00f22e49f05232168cbe
Lucas Fonseca <lucas@lcfcompany.com.br>
2017-07-13 18:20:26 +0000

    first

--- ExternMergeSort.cpp
+++ ExternMergeSort.cpp
@@ -17,4 +17,46 @@ struct Data {
 
 bool intercalaBloco(std::ifstream auxE[2], std::ofstream auxS[2], int stage, int result) {
 
+    bool intercalou = false;
+    Data data[2];
+    int position[2] = {0,0};
+    bool validation[2] = {false,false};
+
+    while((position[0] < stage) && (2 * stage)) {
+        if((position[0] < stage) && !validation[0]) {
+            if(auxE[0].read((char*) &data[0], sizeof(Data))) {
+                validation[0] = true;
+            }else {
+                position[0] = stage;
+            }
+        }
+        if((position[1] < stage) && !validation[1]) {
+            if(auxE[1].read((char*) &data[1], sizeof(Data))) {
+                validation[1] = true;
+            }else {
+                position[1] = stage;
+            }
+        }
+    }
+
+    if(validation[0] && validation[1]) {
+        intercalou = true;
+        if(data[0].key_1 <= data[1].key_1) {
+            auxS[result].write((const char*)(&data[0]), sizeof(Data));
+            validation[0] = false;
+            position[0]++;
+        }else {
+            auxS[result].write((const char*)(&data[1]), sizeof(Data)));
+            validation[1] = false;
+            position[1]++;
+        }
+    }else if(validation[0]){
+        intercalou = true;
+        auxS[result].write((const char*)(&data[0]), sizeof(Data));
+        validation[0] = false;
+        position[0]++;
+    }else if(validation[1]) {
+        intercalou = true;
+        auxS[result].write((const char*))
+    }
 }
